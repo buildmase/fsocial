66122af45290695713a0c03bbf05f099301bdc46
buildmase <mason.earl@icloud.com>
2026-01-11 10:20:31 -0700

    Initial Commit

---
+++ fsocial/fsocialApp.swift
@@ -0,0 +1,17 @@
+//
+//  fsocialApp.swift
+//  fsocial
+//
+//  Created by Mason Earl on 1/11/26.
+//
+
+import SwiftUI
+
+@main
+struct fsocialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
